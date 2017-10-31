9c2a2c5b3d57c11aa4765f1b7977a0f59927081f
Suyash <suyash342@gmail.com>
2017-10-31 19:32:11 +0530

    First Commit

---
+++ app/src/main/java/com/example/suyesh/minotech/SplashScreen.java
@@ -0,0 +1,47 @@
+package com.example.suyesh.minotech;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private ImageView ivLogo;
+    private TextView tvText,tvSlogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        ivLogo = (ImageView)findViewById(R.id.ivLogo);
+        tvText = (TextView)findViewById(R.id.tvText);
+        tvSlogan = (TextView)findViewById(R.id.tvSlogan);
+        Animation myAnim = AnimationUtils.loadAnimation(this,R.anim.mytransition);
+        tvText.startAnimation(myAnim);
+        ivLogo.startAnimation(myAnim);
+        tvSlogan.startAnimation(myAnim);
+        final Intent i = new Intent(this,MainPage.class);
+        Thread timer = new Thread(){
+            public void run(){
+                try {
+                    sleep(6000);
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+        }
+
+}
